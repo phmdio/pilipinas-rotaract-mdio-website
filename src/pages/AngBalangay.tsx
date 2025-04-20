f0da71647563e0d3a9d0643d9b375e0ad3e8a5f6
JHNLWHD <aljhoenilw@gmail.com>
2025-04-21 03:45:04 +0800

    feat: update images and styling ang balangay page

--- src/pages/AngBalangay.tsx
+++ src/pages/AngBalangay.tsx
@@ -4,6 +4,8 @@ import { Helmet } from 'react-helmet-async';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import PageHero from '@/components/PageHero';
+import { Link } from 'react-router-dom';
+import { Separator } from '@/components/ui/separator';
 
 const AngBalangay = () => {
   return (
@@ -21,7 +23,7 @@ const AngBalangay = () => {
       <main>
         <PageHero 
           title="Ang Balangay" 
-          backgroundImage="/lovable-uploads/cdbe779e-203e-4917-979f-94860c8966c4.png"
+          backgroundImage="/assets/masquerade.png"
         />
         
         <section className="py-16">
@@ -30,7 +32,7 @@ const AngBalangay = () => {
               {/* Newsletter Image */}
               <div className="rounded-lg overflow-hidden">
                 <img 
-                  src="/lovable-uploads/cdbe779e-203e-4917-979f-94860c8966c4.png"
+                  src="/assets/carousel.png"
                   alt="Ang Balangay Newsletter"
                   className="w-full h-auto object-cover"
                 />
@@ -41,18 +43,19 @@ const AngBalangay = () => {
                 <h2 className="text-3xl font-bold text-rotaract-magenta mb-2">
                   Ang Balangay
                 </h2>
-                <p className="text-sm text-gray-600 mb-6">
+                <p className="text-sm text-black mb-6">
                   Volume 2 Issue No.1
                 </p>
-                <p className="text-gray-700 mb-6">
+                <Separator className="bg-black mb-6" />
+                <p className="text-black mb-6">
                   It has always been a dream for the Philippine District Rotaract newsletter to have its own publication. With all the programs and activities of clubs, there were numerous generations of PDRRs who aspired to achieve it's milestone. It was merely a dream that was long until such a time when the Philippine Rotaract was given a chance to have the full support of the people behind it who saw the urge and wanted to amplify all the voices from the field as our very own service to fulfill the commitment to our organization be seen. With unity, determination and hard work, Philippine became the strongest force thru made the Philippine District in a whole.
                 </p>
-                <a 
-                  href="/under-construction"
-                  className="btn-rotaract inline-block"
+                <Link 
+                  to="/ang-balangay"
+                  className="btn-rotaract inline-block rounded-full"
                 >
                   READ ANG BALANGAY HERE
-                </a>
+                </Link>
               </div>
             </div>
           </div>
