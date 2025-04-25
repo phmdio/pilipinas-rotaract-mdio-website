ec62a5e75a3fe6a04ba590095244b4d766b04875
JHNLWHD <aljhoenilw@gmail.com>
2025-04-26 02:47:27 +0800

    fix: server-side rendering issue

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -3,12 +3,13 @@ import { ViteApp } from './main'
 import { StaticRouter } from 'react-router-dom/server'
 // Import HelmetProvider directly from react-helmet-async
 import { HelmetProvider } from 'react-helmet-async'
+import { renderToString } from 'react-dom/server'
 
 // Create a helmet context for SSR
 const helmetContext = {}
 
 export function render(url: string) {
-  return (
+  return renderToString(
     <StaticRouter location={url}>
       <HelmetProvider context={helmetContext}>
         <ViteApp />
